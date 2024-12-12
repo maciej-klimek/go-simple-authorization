ed27a46154f50a7305dc68bbfcaca64099051a6e
SzupanBurliga <szburliga@gmail.com>
2024-12-12 19:46:54 +0100

    update

--- handlers/content.go
+++ handlers/content.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"simpleAuth/services"
 )
 
@@ -23,16 +26,46 @@ func content(wrt http.ResponseWriter, req *http.Request) {
 	}
 
 	if req.Method == http.MethodPost {
-		Log.Debug("Handling POST request")
+		Log.Debug("Handling POST request for file upload")
 
 		if err := services.Authorize(req); err != nil {
 			Log.Warn("Authorization failed:", err)
 			http.Redirect(wrt, req, "/login", http.StatusFound)
 			return
 		}
+		err := req.ParseMultipartForm(10 << 20)
+		if err != nil {
+			Log.Error("Error parsing multipart form:", err)
+			http.Error(wrt, "Error processing file", http.StatusInternalServerError)
+			return
+		}
 
-		Log.Infof("POST request handled successfully")
-		wrt.Write([]byte("POST request handled successfully!"))
+		file, handler, err := req.FormFile("file")
+		if err != nil {
+			Log.Error("Error retrieving file:", err)
+			http.Error(wrt, "Error retrieving file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+
+		dst := filepath.Join("/shared-data", handler.Filename)
+		out, err := os.Create(dst)
+		if err != nil {
+			Log.Error("Error saving file:", err)
+			http.Error(wrt, "Error saving file", http.StatusInternalServerError)
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			Log.Error("Error copying file:", err)
+			http.Error(wrt, "Error saving file", http.StatusInternalServerError)
+			return
+		}
+
+		Log.Infof("File %s uploaded successfully", handler.Filename)
+		wrt.Write([]byte("File uploaded successfully!"))
 		return
 	}
 }
